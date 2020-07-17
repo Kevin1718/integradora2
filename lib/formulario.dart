9183065c5bfff506f12536c8c778f35fa8b6ac44
Kevin1718 <51490042+Kevin1718@users.noreply.github.com>
2020-07-17 14:53:09 -0500

    app integradora

---
+++ lib/formulario.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class FormCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      width: double.infinity,
+
+      padding: EdgeInsets.only(bottom: 1),
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(8.0),
+          boxShadow: [
+            BoxShadow(
+                color: Colors.black,
+                offset: Offset(0.0, 15.0),
+                blurRadius: 15.0),
+            BoxShadow(
+                color: Colors.black,
+                offset: Offset(0.0, -10.0),
+                blurRadius: 10.0),
+          ]),
+      child: Padding(
+        padding: EdgeInsets.only(left: 16.0, right: 16.0, top: 16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text("Login",
+                style: TextStyle(
+                    fontSize: ScreenUtil.getInstance().setSp(45),
+                    fontFamily: "Poppins-Bold",
+                    letterSpacing: .6)),
+            SizedBox(
+              height: ScreenUtil.getInstance().setHeight(30),
+            ),
+            Text("Usuario",
+                style: TextStyle(
+                    fontFamily: "Poppins-Medium",
+                    fontSize: ScreenUtil.getInstance().setSp(26))),
+            TextField(
+              decoration: InputDecoration(
+
+                  hintStyle: TextStyle(color: Colors.black, fontSize: 12.0)),
+            ),
+            SizedBox(
+              height: ScreenUtil.getInstance().setHeight(30),
+            ),
+            Text("Contraseña",
+                style: TextStyle(
+                    fontFamily: "Poppins-Medium",
+                    fontSize: ScreenUtil.getInstance().setSp(26))),
+            TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+
+                  hintStyle: TextStyle(color: Colors.black, fontSize: 12.0)),
+            ),
+            SizedBox(
+              height: ScreenUtil.getInstance().setHeight(35),
+            ),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
